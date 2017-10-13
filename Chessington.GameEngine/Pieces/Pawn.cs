0e9db23e240270470b7d805611ee1d7ee13b960f
Tara <tburdittx@live.com>
2017-10-13 14:44:18 +0100

    Passed first lot of tests

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,12 +5,23 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            List<Square> Moves = new List<Square>();
+            if (Player==Player.White) 
+            {
+                Moves.Add(new Square(position.Row - 1, position.Col));
+            }
+
+            if (Player==Player.Black)
+            {
+                Moves.Add(new Square(position.Row + 1, position.Col));
+            }
+            return Moves;
         }
     }
 }
