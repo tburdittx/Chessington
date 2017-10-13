06a627a261967eca021f7197fa285fb281073844
Tara <tburdittx@live.com>
2017-10-13 16:37:36 +0100

    Queen moves in any direction

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,32 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            List<Square> Moves = new List<Square>();
+
+           // if moving diagonally
+            {
+                for (var i = 0; i < 8; i++)
+                    Moves.Add(Square.At(i, i));
+
+                for (var i = 1; i < 8; i++)
+                    Moves.Add(Square.At(i, 8 - i));
+            }
+
+           // if moving laterally
+            {
+                for (var i = 0; i < 8; i++)
+                    Moves.Add(Square.At(position.Row, i));
+
+                for (var i = 0; i < 8; i++)
+                    Moves.Add(Square.At(i, position.Col));
+            }
+
+            Moves.RemoveAll(s => s == Square.At(position.Row, position.Col));
+
+            return Moves;
+
+
         }
     }
 }
